6cefba26fc9f317d4b14e622e7b0f6843385385f
cracker64 <cracker642@gmail.com>
2015-03-27 01:10:38 -0400

    PHP version of this too, apparently

---
+++ algersoft.net/login/krieger/projects/arson simulation.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="We don't ask what you'll use it for. #ArcherFX">
+<meta name="keywords" content="SOFTWARE, HARDWARE, ROBOTICS, CYBORNETICS, CLONING, HOLOGRAPHY, DRUG THERAPY, TENTACLES, KRIEGERANDS">
+<meta name="author" content="Dr. Algernop Krieger">
+
+<meta property="og:title" content="Algersoft" />
+<meta property="og:image" content="http://algersoft.net/hero_facebook.jpg" />
+<meta property="og:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta property="og:url" content="http://algersoft.net" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@KriegerSignals" />
+<meta name="twitter:title" content="Algersoft" />
+<meta name="twitter:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta name="twitter:image" content="http://algersoft.net/hero_twitter.jpg" />
+<meta name="twitter:url" content="http://algersoft.net" />
+
+<meta name="viewport" content = "width = 900">
+<meta name="apple-mobile-web-app-title" content="Algersoft">
+
+<title>Algersoft</title>
+
+<!--MNZWFTXGKUDBRVAJMYJUCNKKW--!>
+
+<link rel="image_src" href="http://algersoft.net/hero_facebook.jpg" / >
+<link href="/stylesheet.css" rel="stylesheet" type="text/css">
+<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59929264-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+</head>
+<body>
+
+<div class="main_menu_container">
+  	<ul id="main_menu">
+  		
+				<li><a href="/">HOME</a></li>
+							<li><a href="/about/">ABOUT</a></li>
+							<li><a href="/expertise/">EXPERTISE</a></li>
+							<li><a href="/testimonial/">TESTIMONIAL</a></li>
+							<li><a href="/contact/">CONTACT</a></li>
+							<li><a href="/login/">LOG IN</a></li>
+				
+	</ul>
+</div><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id="logo"></div>
+<!--<img src="/images/logo.png" width="846" height="180" alt="ALGERSOFT"/>--><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta http-equiv="pragma" content="no-cache"/>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <!-- <meta name ="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /> -->
+        <meta name = "viewport" content = "width = 640">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta charset="utf-8"/>
+		<link rel="stylesheet" type="text/css" href="game_stylesheet.css">
+        
+		<meta name="author" content="Dr. Algernop Krieger">
+		<meta name="description" content="A video game featuring arson enthusiast Cheryl Tunt. #ArcherFX">
+
+		<meta property="og:title" content="YOU'RE NOT MY SUPERVISOR!!">
+		<meta property="og:image" content="http://algersoft.net/hero_ynms_facebook.jpg">
+		<meta property="og:description" content="A video game featuring arson enthusiast Cheryl Tunt. #ArcherFX">
+        <meta property="og:url" content="http://algersoft.net/login/krieger/projects/arson%20simulation.html" />
+
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@KriegerSignals" />
+		<meta name="twitter:title" content="YOU'RE NOT MY SUPERVISOR!!" />
+		<meta name="twitter:description" content="A video game featuring arson enthusiast Cheryl Tunt. #ArcherFX" />
+		<meta name="twitter:image" content="http://algersoft.net/hero_ynms_twitter.jpg" />
+		<meta name="twitter:url" content="http://algersoft.net/login/krieger/projects/arson%20simulation.html" />
+
+	<!-- Game by Tim Farrell -->
+	<!-- Music by Myke Chapman -->
+	<!-- Titles by Mark Paterson -->
+	
+        <!-- Set the title bar of the page -->
+        <title>YOU'RE NOT MY SUPERVISOR!!</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59929264-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</head>
+
+<body>
+
+		<div class="gm4html5_div_class" id="gm4html5_div_id">
+            <canvas id="canvas" width="640" height="512">
+               <p>Your browser doesn't support HTML5 canvas.</p>
+            </canvas>
+        </div>
+        <script type="text/javascript" src="html5game/Cheryl_Original.js?KRDYB=787844837"></script>
+        
+</body>
+<!-- MADE IN GEORGIA :p -->
+</html>
