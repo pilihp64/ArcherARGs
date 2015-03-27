ea4c046f5af7bfc5f939ab2a2491fe7d371566ec
cracker64 <cracker642@gmail.com>
2015-03-27 01:20:05 -0400

    We should track the root index pages too.

---
+++ algersoft.net/expertise/index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="We don't ask what you'll use it for. #ArcherFX">
+<meta name="keywords" content="SOFTWARE, HARDWARE, ROBOTICS, CYBORNETICS, CLONING, HOLOGRAPHY, DRUG THERAPY, TENTACLES, KRIEGERANDS">
+<meta name="author" content="Dr. Algernop Krieger">
+
+<meta property="og:title" content="Algersoft" />
+<meta property="og:image" content="http://algersoft.net/hero_facebook.jpg" />
+<meta property="og:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta property="og:url" content="http://algersoft.net" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@KriegerSignals" />
+<meta name="twitter:title" content="Algersoft" />
+<meta name="twitter:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta name="twitter:image" content="http://algersoft.net/hero_twitter.jpg" />
+<meta name="twitter:url" content="http://algersoft.net" />
+
+<meta name="viewport" content = "width = 900">
+<meta name="apple-mobile-web-app-title" content="Algersoft">
+
+<title>Algersoft</title>
+
+<!--MNZWFTXGKUDBRVAJMYJUCNKKW--!>
+
+<link rel="image_src" href="http://algersoft.net/hero_facebook.jpg" / >
+<link href="/stylesheet.css" rel="stylesheet" type="text/css">
+<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59929264-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+</head>
+<body>
+
+<div class="main_menu_container">
+  	<ul id="main_menu">
+  		
+				<li><a href="/">HOME</a></li>
+							<li><a href="/about/">ABOUT</a></li>
+							<li><a href="/expertise/">EXPERTISE</a></li>
+							<li><a href="/testimonial/">TESTIMONIAL</a></li>
+							<li><a href="/contact/">CONTACT</a></li>
+							<li><a href="/login/">LOG IN</a></li>
+				
+	</ul>
+</div><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div id="logo"></div>
+<!--<img src="/images/logo.png" width="846" height="180" alt="ALGERSOFT"/>--><br>
+<br>
+<br>
+<br>
+<br>
+<div id="box_title_container">
+	<div id="box_title">EXPERTISE
+    </div>
+</div>
+<div id="box">
+	<div id="box_text">
+	  <ul>
+	    <li>Bespoke software & hardware solutions</li>
+	    <li>Automobile Field Modification</li>
+	    <li>Submersibles </li>
+	    <li>Biometrics</li>
+	    <li>Robotics</li>
+	    <li>Biological Enhancement</li>
+	    <li>Cyborg Enhancement</li>
+	    <li>Experimental Drug Therapy</li>
+	    <li>Advanced Holography</li>
+	    <li>Human & Animal cloning</li>
+	    <li>Secure Cloud Computing (surpassing D.o.D specifications)</li>
+	    <li>Unmanned Systems</li>
+	    <li>Weapons Research</li>
+	    <li>Mainframe Technical Support</li>
+        <li>Kriegerand Mining Hardware (Liquid Cooled A.S.I.C)</li>
+        <li>Safe room consultancy</li>
+      </ul>
+	</div>
+</div>
+<br><br><br><br>
+</body>
+</html>
