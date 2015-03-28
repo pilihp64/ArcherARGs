51c2616bc80126ece4de21ff7f9268086cf65231
cracker64 <cracker642@gmail.com>
2015-03-28 17:53:33 -0400

    Manually add the WORM php index, second logo image.

---
+++ algersoft.net/index_WORM.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="We don't ask what you'll use it for. #ArcherFX">
+<meta name="keywords" content="SOFTWARE, HARDWARE, ROBOTICS, CYBORNETICS, CLONING, HOLOGRAPHY, DRUG THERAPY, TENTACLES, KRIEGERANDS">
+<meta name="author" content="Dr. Algernop Krieger">
+
+<meta property="og:title" content="Algersoft" />
+<meta property="og:image" content="http://algersoft.net/hero_facebook.jpg" />
+<meta property="og:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta property="og:url" content="http://algersoft.net" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@KriegerSignals" />
+<meta name="twitter:title" content="Algersoft" />
+<meta name="twitter:description" content="We don't ask what you'll use it for. #ArcherFX" />
+<meta name="twitter:image" content="http://algersoft.net/hero_twitter.jpg" />
+<meta name="twitter:url" content="http://algersoft.net" />
+
+<meta name="viewport" content = "width = 900">
+<meta name="apple-mobile-web-app-title" content="Algersoft">
+
+<title>Algersoft</title>
+
+<!--MNZWFTXGKUDBRVAJMYJUCNKKW--!>
+
+<link rel="image_src" href="http://algersoft.net/hero_facebook.jpg" / >
+<link href="/stylesheet.css" rel="stylesheet" type="text/css">
+<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59929264-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+</head>
+<body> 
+<div class="main_menu_container">
+        <ul id="main_menu">
+              
+                                <li><a href="/">HOME</a></li>
+                                                <li><a href="/about/">ABOUT</a></li>
+                                                <li><a href="/expertise/">EXPERTISE</a></li>
+                                                <li><a href="/testimonial/">TESTIMONIAL</a></li>
+                                                <li><a href="/contact/">CONTACT</a></li>
+                                                <li><a href="/login/">LOG IN</a></li>
+                                </ul>
+</div><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<style>
+	#logo_glitch {
+	display: block;
+	width: 846px;
+	height: 180px;
+	background-image: url(worm/logo_glitch_2.png);
+	background-repeat: no-repeat;
+	margin: 0 auto;
+	position: relative;
+}
+</style>
+<div id="logo_glitch"></div><br>
+<br>
+<br>
+<br>
+<br>
+<span id="tagline">934 ERROR. MAINFRAME MALFUNCTION.</span>
+<br>
+<img src="worm/worm.gif" width="800" height="400" alt="" loop=infinite/>
+<br><br><br><br>
+ <audio autoplay loop>
+  <source src="worm/worm.mp3" type="audio/mpeg">
+</audio> 
+</body>
+<!-- MADE IN GEORGIA :p -->
+</html>
